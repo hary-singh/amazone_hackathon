13cf08f5db8098a34d170ff6c74ff353f37184e7
Henry Doan <henrydoan96@gmail.com>
2021-03-11 19:16:57 -0700

    auth working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
-  before_action :render_navbar
 
   protected 
-    def render_navbar
-      render component: "Navbar"
-    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   devise_for :users
-
+  get '/home', to: 'static_pages#home'
 end
