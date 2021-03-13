d791803c058592563b1a57718474aa41e4f8dc59
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-13 10:41:10 -0700

    controller for product created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/edit'
+  get 'products/create'
+  get 'products/update'
+  get 'products/destroy'
   root 'static_pages#home'
   devise_for :users
   get '/home', to: 'static_pages#home'
