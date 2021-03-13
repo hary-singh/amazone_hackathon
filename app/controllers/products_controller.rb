e8feec0e0fcfc23461a98a3e1ef9055fa5034575
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-13 11:15:54 -0700

    product controller completed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,26 +6,45 @@ class ProductsController < ApplicationController
   end
 
   def show
+    render component: "Product", props: { product: @product }
   end
 
   def new
+    @product = Product.new
+    render component: "ProductNew", props: { product: @product }
   end
 
   def edit
+    render component: "ProductEdit", props: { product: @product }
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render component: "ProductNew", props: { product: @product }
   end
 
   def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render component: "ProductEdit", props: { product: @product } 
   end
 
   def destroy
+    @product.destroy
+    redirect_to products_path
   end
 
   private
     def set_product
       @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name)
     end
 
 end
