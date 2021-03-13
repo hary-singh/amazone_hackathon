42830f90a4ff06ad5300052c7b89c097b073c564
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-13 12:00:52 -0700

    updated department error

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  
+
+  resources :departments do
+    resources :products
+  end
+  root 'static_pages#home'
+  devise_for :users
+  get '/home', to: 'static_pages#home'
 end
